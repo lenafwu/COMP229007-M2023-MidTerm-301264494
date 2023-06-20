8faa610f34be60e3e84ae83f8c24dc74505c2be8
LenaFWU <lena.f.wu@gmail.com>
2023-06-20 15:33:21 -0400

    fixed sign up issue

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # COMP229.M2023.Midterm.Template
- COMP229.M2023 - Midterm Template
+
+COMP229.M2023 - Midterm Template
 
 6-20 Known issue:
-Cars drop down menu doesn't show:
-Fixed: footer.ejs: include bootstrap.bundle.min.js instead of bootstrap.min.js
-Reference: https://stackoverflow.com/questions/68750341/bootstrap-5-uncaught-typeerror-popper-namespace-createpopper-is-not-a-functi
+
+1. Cars drop down menu doesn't show:
+   Problem: popper.js not included
+   Fixed: footer.ejs: include bootstrap.bundle.min.js instead of bootstrap.min.js
+   Reference: https://stackoverflow.com/questions/68750341/bootstrap-5-uncaught-typeerror-popper-namespace-createpopper-is-not-a-functi
+
+2. Sign up not working
+   Problem: MongooseError: Model.prototype.save() no longer accepts a callback
+   Fixed: controllers/user.js: updated to async function
--- controllers/user.js
+++ controllers/user.js
@@ -1,18 +1,18 @@
-let User = require('../models/user');
-let passport = require('passport');
+let User = require("../models/user");
+let passport = require("passport");
 
 function getErrorMessage(err) {
   console.log("===> Erro: " + err);
-  let message = '';
+  let message = "";
 
   if (err.code) {
     switch (err.code) {
       case 11000:
       case 11001:
-        message = 'Username already exists';
+        message = "Username already exists";
         break;
       default:
-        message = 'Something went wrong';
+        message = "Something went wrong";
     }
   } else {
     for (var errName in err.errors) {
@@ -21,75 +21,94 @@ function getErrorMessage(err) {
   }
 
   return message;
-};
+}
 
-module.exports.renderSignin = function(req, res, next) {
+module.exports.renderSignin = function (req, res, next) {
   if (!req.user) {
-    res.render('auth/signin', {
-      title: 'Sign-in Form',
-      messages: req.flash('error') || req.flash('info')
+    res.render("auth/signin", {
+      title: "Sign-in Form",
+      messages: req.flash("error") || req.flash("info"),
     });
   } else {
     console.log(req.user);
-    return res.redirect('/');
+    return res.redirect("/");
   }
 };
 
-module.exports.renderSignup = function(req, res, next) {
+module.exports.renderSignup = function (req, res, next) {
   if (!req.user) {
-
     // creates a empty new user object.
     let newUser = User();
 
-    res.render('auth/signup', {
-      title: 'Sign-up Form',
-      messages: req.flash('error'),
-      user: newUser
+    res.render("auth/signup", {
+      title: "Sign-up Form",
+      messages: req.flash("error"),
+      user: newUser,
     });
-
   } else {
-    return res.redirect('/');
+    return res.redirect("/");
   }
 };
 
-module.exports.signup = function(req, res, next) {
+// FIXME: OK: change to async function which returns a promise so it works properly
+module.exports.signup = async function (req, res, next) {
   if (!req.user && req.body.password === req.body.password_confirm) {
-    console.log(req.body);
+    console.log("req.body: ", req.body);
 
     let user = new User(req.body);
-    console.log(user);
+    console.log("user: ", user);
 
+    try {
+      const savedUser = await user.save();
+      req.login(savedUser, (err) => {
+        if (err) return next(err);
+        return res.redirect("/");
+      });
+    } catch (err) {
+      let message = getErrorMessage(err);
+      req.flash("error", message);
+      return res.render("auth/signup", {
+        title: "Sign-up Form",
+        messages: req.flash("error"),
+        user: user,
+      });
+    }
+  } else {
+    return res.redirect("/");
+  }
+
+  /* original code: 
     user.save((err) => {
       if (err) {
         let message = getErrorMessage(err);
 
-        req.flash('error', message);
-        return res.render('auth/signup', {
-          title: 'Sign-up Form',
-          messages: req.flash('error'),
-          user: user
+        req.flash("error", message);
+        return res.render("auth/signup", {
+          title: "Sign-up Form",
+          messages: req.flash("error"),
+          user: user,
         });
       }
       req.login(user, (err) => {
         if (err) return next(err);
-        return res.redirect('/');
+        return res.redirect("/");
       });
     });
   } else {
-    return res.redirect('/');
-  }
+    return res.redirect("/");
+  } */
 };
 
-module.exports.signout = function(req, res, next) {
+module.exports.signout = function (req, res, next) {
   req.logout();
-  res.redirect('/');
+  res.redirect("/");
 };
 
-module.exports.signin = function(req, res, next){
-  passport.authenticate('local', {   
-    successRedirect: req.session.url || '/',
-    failureRedirect: '/users/signin',
-    failureFlash: true
+module.exports.signin = function (req, res, next) {
+  passport.authenticate("local", {
+    successRedirect: req.session.url || "/",
+    failureRedirect: "/users/signin",
+    failureFlash: true,
   })(req, res, next);
   delete req.session.url;
-}
+};
